b7a03f9f7bada3287c01b9a27d7040a6339f4c4c
Alex Alvarez <doubleabatteryalvarez@gmail.com>
2019-09-30 00:12:43 -0600

    Initial Commit
    Basic Mech Movement

---
+++ Assets/MachineGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGun : PrimaryWeapon
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Fire()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Reload()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/PrimaryWeapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PrimaryWeapon : MonoBehaviour
+{
+    int ammo;
+
+    public abstract void Fire();
+    public abstract void Reload();
+}
